8e3d909321fbd6b8a97666900d6aa7ec990478d8
Julian <julian.cleaud@gmail.com>
2018-03-11 03:48:52 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,35 @@
+# ZSH fast command-not-found plugin for OSX
+
+The purpose of this plugin is to provide a faster alternative to
+oh-my-zsh / prezto ones, which run multiple useless checks, and
+run brew command within zshrc.
+
+The problem is brew command are really slow (0.5~2s) and causes
+huge slow to zsh load time.
+
+This plugin check for brew command-not-found dynamically, when
+a wrong command has been typed. This cause a latency each time a
+a not found command is typed.
+However, given the huge auto-correction possibility and the
+syntax highlighting features of zsh, this is something that
+rarely happens, and which is less disturbing because we can stop
+it with <ctrl>+<v> easily.
+
+
+## Installation
+
+You can use this plugin using zplug :
+
+```bash
+zplug "Tireg/zsh-macos-command-not-found", if:"[[ "${OSTYPE}" == 'darwin'* ]]";
+```
+
+
+## Requirements
+
+This plugin is only meant to be sourced on macOS. Be sure to check
+your OS before sourcing it (like in Installation step with zplug)
+
+You should install [`brew`](https://brew.sh/index_fr), and tap
+[`homebrew/command-not-found`](https://github.com/Homebrew/homebrew-command-not-found)
+to make this plugin useful.
---
+++ macos-command-not-found.plugin.zsh
@@ -0,0 +1,36 @@
+# ------------------------------------------------------------------------------
+#          FILE:  macos-command-not-found.plugin.zsh
+#   DESCRIPTION:  MacOS plugin that emulates command-not-found like in Ubuntu,
+#                 meaning it finds and prompts which package must be installed
+#                 to get the currently not found command.
+#        AUTHOR:  Tireg
+#   INSPIRED BY:  https://github.com/robbyrussell/oh-my-zsh/tree/master/plugins/command-not-found
+#       VERSION:  1.0.0
+# ------------------------------------------------------------------------------
+
+function command_not_found_handler() {
+	local cmd="$1";
+
+	# The code below is based off this Linux Journal article:
+	#   http://www.linuxjournal.com/content/bash-command-not-found
+
+	# Do not run when inside Midnight Commander or within a Pipe,
+	# except if on Travis-CI.
+	# Then check for brew command-not-found tap
+	test -z "${CONTINUOUS_INTEGRATION}" \
+		&& ! test -n "${NC_SID}" -o ! -t 1 \
+		&& (( $+commands[brew] )) \
+		&& brew command which-formula >/dev/null \
+		&& {
+			# Brew command-not-found exists, so we can use it
+			local txt="$(brew which-formula --explain "${cmd}" 2>/dev/null)";
+			# If formula has been found, print instructions
+			[ ! -z "${txt}" ] && echo "${txt}";
+		} \
+		|| {
+			# Zsh versions 5.3 and above don't print this for us.
+			[[ "${ZSH_VERSION}" > "5.2" ]] \
+				&& echo "zsh: command not found: ${cmd}";
+		}
+	return 127;
+}
