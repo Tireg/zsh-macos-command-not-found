f18e8adced34242d756778d7fb6ca5f6ff8e26fe
Julian <julian.cleaud@gmail.com>
2018-03-13 20:33:57 +0100

    Use variable instead of zstyle for cache

--- macos-command-not-found.plugin.zsh
+++ macos-command-not-found.plugin.zsh
@@ -10,33 +10,36 @@
 
 function command_not_found_handler() {
 	local cmd="$1";
+	typeset -gi _CMDNF_SKIP_BREW;
 
-	zstyle -t ':tireg:module:command-not-found' use-brew;
-	local SKIP_BREW="$?";
-
-	# Do not run when inside Midnight Commander or within a Pipe,
-	# except if on Travis-CI.
-	# Then check for brew command-not-found tap
-	[[ "${SKIP_BREW}" -eq 2 ]] \
-		&& test -z "${CONTINUOUS_INTEGRATION}" \
-		&& ! test -n "${NC_SID}" -o ! -t 1 \
-		&& (( $+commands[brew] )) \
-		&& zstyle ':tireg:module:command-not-found' use-brew true \
-		&& SKIP_BREW="0";
+	# Initialize cache if not set
+	if [ -z "${_CMDNF_SKIP_BREW}" ]; then
+		# Do not run when inside Midnight Commander or within a Pipe,
+		# except if on Travis-CI.
+		# Then check for brew command-not-found tap
+		if   test -z "${CONTINUOUS_INTEGRATION}" && \
+		   ! test -n "${NC_SID}" -o ! -t 1 && \
+		   (( ${+commands[brew]} ));
+		then
+			_CMDNF_SKIP_BREW="0";
+		else
+			_CMDNF_SKIP_BREW="1";
+		fi
+	fi
 
 	# The code below is based off this Linux Journal article:
 	#   http://www.linuxjournal.com/content/bash-command-not-found
-
-	! (( ${SKIP_BREW} )) && {
+	(( ${SKIP_BREW} )) && {
+		# Zsh versions 5.3 and above don't print this for us.
+		[[ "${ZSH_VERSION}" > "5.2" ]] \
+			&& echo "zsh: command not found: ${cmd}";
+	} || {
 		# Brew command-not-found exists, so we can use it
 		local txt="$(brew which-formula --explain "${cmd}" 2>/dev/null)";
 		# If formula has been found, print instructions
 		[ ! -z "${txt}" ] && echo "${txt}";
-	} \
-	|| {
-		# Zsh versions 5.3 and above don't print this for us.
-		[[ "${ZSH_VERSION}" > "5.2" ]] \
-			&& echo "zsh: command not found: ${cmd}";
 	}
+
+	# Return command not found error code
 	return 127;
 }
